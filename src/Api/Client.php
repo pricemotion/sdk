a142901e47d520a17578cda3af7a236e40b0a9c2
Albert Peschar <albert@peschar.net>
2022-03-29 16:47:24 +0300

    Add Client::getProduct

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -4,6 +4,7 @@ namespace Pricemotion\Sdk\Api;
 
 use Pricemotion\Sdk\Data\Ean;
 use Pricemotion\Sdk\Data\EanCollection;
+use Pricemotion\Sdk\Data\Product;
 use Pricemotion\Sdk\RuntimeException;
 
 class Client {
@@ -16,27 +17,50 @@ class Client {
     }
 
     public function followProducts(EanCollection $eans): FollowProductsResponse {
+        $response = $this->curlRequest([
+            CURLOPT_URL => 'https://www.pricemotion.nl/api/product/follow',
+            CURLOPT_USERPWD => "{$this->apiKey}:",
+            CURLOPT_POSTFIELDS => json_encode(
+                array_map(fn(Ean $ean) => $ean->toString(), $eans->toArray()),
+                JSON_THROW_ON_ERROR,
+            ),
+        ]);
+        $data = json_decode($response, true, 512, JSON_THROW_ON_ERROR);
+        return new FollowProductsResponse($data);
+    }
+
+    public function getProduct(Ean $ean): Product {
+        $response = $this->curlRequest([
+            CURLOPT_URL =>
+                'https://www.pricemotion.nl/service/?' .
+                http_build_query([
+                    'serial' => $this->apiKey,
+                    'ean' => $ean->toString(),
+                ]),
+        ]);
+
+        $dom = new \DOMDocument();
+        if (!$dom->loadXML($response)) {
+            throw new RuntimeException('Could not load Pricemotion product XML');
+        }
+
+        return Product::fromXmlResponse($dom);
+    }
+
+    private function curlRequest(array $options): string {
+        $options += [
+            CURLOPT_FAILONERROR => true,
+            CURLOPT_RETURNTRANSFER => true,
+        ];
         $ch = $this->getCurlHandle();
-        if (
-            !curl_setopt_array($ch, [
-                CURLOPT_URL => 'https://www.pricemotion.nl/api/product/follow',
-                CURLOPT_USERPWD => "{$this->apiKey}:",
-                CURLOPT_POSTFIELDS => json_encode(
-                    array_map(fn(Ean $ean) => $ean->toString(), $eans->toArray()),
-                    JSON_THROW_ON_ERROR,
-                ),
-                CURLOPT_FAILONERROR => true,
-                CURLOPT_RETURNTRANSFER => true,
-            ])
-        ) {
+        if (!curl_setopt_array($ch, $options)) {
             throw new RuntimeException('curl_setopt_array failed');
         }
         $response = curl_exec($ch);
         if ($response === false) {
             throw new RuntimeException(sprintf('curl_exec failed: (%d) %s', curl_errno($ch), curl_error($ch)));
         }
-        $data = json_decode($response, true, 512, JSON_THROW_ON_ERROR);
-        return new FollowProductsResponse($data);
+        return $response;
     }
 
     private function getCurlHandle() {
--- test/Api/ClientTest.php
+++ test/Api/ClientTest.php
@@ -4,20 +4,25 @@ namespace Pricemotion\Sdk\Api;
 
 use PHPUnit\Framework\TestCase;
 use Pricemotion\Sdk\Data\EanCollection;
+use Pricemotion\Sdk\Data\Ean;
 
 class ClientTest extends TestCase {
     public function testFollowProducts(): void {
+        $eans = EanCollection::fromStrings(['0077985029579', '5025155025710']);
+        $result = $this->getClient()->followProducts($eans);
+        $this->assertCount(2, $result->getData());
+    }
+
+    public function testGetProduct(): void {
+        $result = $this->getClient()->getProduct(Ean::fromString('0047871061020'));
+        $this->assertGreaterThan(0, $result->getLowestPrice());
+    }
+
+    private function getClient(): Client {
         $apiKey = getenv('PRICEMOTION_API_KEY');
         if (!$apiKey) {
             $this->markTestSkipped('PRICEMOTION_API_KEY is not set');
         }
-
-        $client = new Client($apiKey);
-
-        $eans = EanCollection::fromStrings(['0077985029579', '5025155025710']);
-
-        $result = $client->followProducts($eans);
-
-        $this->assertCount(2, $result->getData());
+        return new Client($apiKey);
     }
 }
